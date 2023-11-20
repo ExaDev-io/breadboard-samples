2962e9a74b6967da7cbebeca402d01d4922374e9
Joseph Mearman <joseph@mearman.co.uk>
2023-11-20 21:30:25 +0000

    Refactor MyComponent.tsx for improved readability and maintainability.
    - Reformat code to adhere to consistent coding style.
    - Add type annotations for better type safety.
    - Extract inline styles into a separate constant for easier modification.
    - Update comments and variable names for clarity.
    - Organize input and output nodes into separate sections with corresponding headings.

--- samples/Hello React Breadboard/src/MyComponent.tsx
+++ samples/Hello React Breadboard/src/MyComponent.tsx
@@ -1,67 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import {Board} from "@google-labs/breadboard"
+import React, { useState, useEffect } from "react";
+import { Board } from "@google-labs/breadboard";
 
-const board  = new Board()
-const input = board.input()
-const output = board.output()
-input.wire("*", output)
-
+const board = new Board();
+const input = board.input();
+const output = board.output();
+input.wire("foo", output);
+const preStyle: React.CSSProperties = {
+	textAlign: "left",
+	whiteSpace: "pre-wrap",
+	wordBreak: "break-word",
+};
 
 const MyComponent = () => {
-    const [inputData, setInputData] = useState({});
-    const [outputData, setOutputData] = useState({});
-    const [userInput, setUserInput] = useState(""); // State to hold user input
+	const [inputData, setInputData] = useState({});
+	const [outputData, setOutputData] = useState({});
+	const [userInput, setUserInput] = useState(""); // State to hold user input
 
-    // Event handler for user input
-    const handleInputChange = (event) => {
-        setUserInput(event.target.value);
-    };
+	// Event handler for user input
+	const handleInputChange = (event: {
+		target: { value: React.SetStateAction<string> };
+	}) => {
+		setUserInput(event.target.value);
+	};
 
-    // Submit handler to initiate processing
-    const handleSubmit = async () => {
-        for await (const run of board.run({})) {
-            if (run.type === "input") {
-                // Use the user input data
-                run.inputs = {
-                    ...run.inputs,
-                    inputKey: userInput, // Assuming 'inputKey' is what you need
-                };
-                setInputData(prev => ({
-                    ...prev,
-                    [run.node.id]: run.inputs,
-                }));
-            }
-            if (run.type === "output") {
-                setOutputData(prev => ({
-                    ...prev,
-                    [run.node.id]: run.outputs,
-                }));
-            }
-        }
-    };
+	// Submit handler to initiate processing
+	const handleSubmit = async () => {
+		for await (const run of board.run({})) {
+			if (run.type === "input") {
+				// Use the user input data
+				run.inputs = {
+					...run.inputs,
+					inputKey: userInput, // Assuming 'inputKey' is what you need
+				};
+				setInputData((prev) => ({
+					...prev,
+					[run.node.id]: {
+						node: run.node,
+						inputs: run.inputs,
+						state: run.state,
+					},
+				}));
+			}
+			if (run.type === "output") {
+				setOutputData((prev) => ({
+					...prev,
+					[run.node.id]: {
+						node: run.node,
+						outputs: run.outputs,
+						state: run.state,
+					},
+				}));
+			}
+		}
+	};
 
-    return (
-        <div>
-            <input type="text" value={userInput} onChange={handleInputChange} />
-            <button onClick={handleSubmit}>Submit</button>
-			<div>
-				<h3>Input data</h3>
-				<pre>{JSON.stringify(inputData, null, 2)}</pre>
+	return (
+		<div>
+			<input type="text" value={userInput} onChange={handleInputChange} />
+			<button onClick={handleSubmit}>Submit</button>
 
-				<h3>Output data</h3>
+			<h2>Input Nodes</h2>
+			<ul>
+				{Object.entries(inputData).map(([id, data]) => (
+					<li key={id}>
+						Input Node ID: {id}
+						<pre style={preStyle}>
+							{JSON.stringify(data, null, 2)}
+						</pre>
+					</li>
+				))}
+			</ul>
 
-				<pre>{JSON.stringify(outputData, null, 2)}</pre>
-
-				{/* <h3>Board</h3> */}
-				{/* <pre>{JSON.stringify(board, null, 2)}</pre> */}
-
-				</div>
-
-
-
-
-        </div>
-    );
+			<h2>Output Nodes</h2>
+			<ul>
+				{Object.entries(outputData).map(([id, data]) => (
+					<li key={id}>
+						Output Node ID: {id}
+						<pre style={preStyle}>
+							{JSON.stringify(data, null, 2)}
+						</pre>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default MyComponent;
