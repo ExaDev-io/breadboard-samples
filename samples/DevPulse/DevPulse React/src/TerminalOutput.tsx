91dc465243d766e4e35d4ce35196ec0636e8089b
Joseph Mearman <joseph@mearman.co.uk>
2023-11-21 22:26:56 +0000

    Add scroll functionality to TerminalOutput component
    - Added a useRef hook to create a reference to the div element.
    - Added an useEffect hook that scrolls the div element to the bottom whenever there is new output.
    - Updated the JSX code to include the ref attribute for the div element.
    
    This commit adds scroll functionality to the TerminalOutput component, allowing users to easily view new output without manually scrolling.

--- samples/DevPulse/DevPulse React/src/TerminalOutput.tsx
+++ samples/DevPulse/DevPulse React/src/TerminalOutput.tsx
@@ -1,10 +1,17 @@
 import { Board } from "@google-labs/breadboard";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const CLAUDE_API_KEY = import.meta.env.VITE_CLAUDE_API_KEY;
 
 export function TerminalOutput({board}: { board: Board }) {
 	const [output, setOutput] = useState<string[]>([]);
+	const ref = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (ref.current) {
+			ref.current.scrollTop = ref.current.scrollHeight;
+		}
+	}, [output]);
 
 	useEffect(() => {
 		const processRun = async () => {
@@ -14,29 +21,51 @@ export function TerminalOutput({board}: { board: Board }) {
 						run.inputs = {CLAUDE_API_KEY};
 					}
 				} else if (run.type === "output") {
+					const outputs = run.outputs
+					const outputMessage = `${run.node.id} ${JSON.stringify(outputs, null, 2).replaceAll("\\n", "\n").replaceAll("\\\"", "\"" )}`
+					console.log(outputMessage)
 					setOutput((prev) => [
 						...prev,
-						...JSON.stringify(run.outputs, null, 2).split("\n"),
-						"-".repeat(80),
+						outputMessage,
+						// ...JSON.stringify(run.outputs, null, 2).split("\n"),
+						`\n${"-".repeat(80)}\n`,
 					]);
 				}
 			}
 		};
-		processRun();
+		processRun().then(r => console.log("processRun", r));
 	}, [board]);
 
 	return (
 		<div
+			ref={ref}
 			style={{
+				// height: '200px',
+				// overflowY: 'auto',
 				backgroundColor: "black",
 				color: "lime",
 				fontFamily: "monospace",
 				padding: "10px",
+				position: 'absolute',
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+				overflowY: 'auto',
 			}}
 		>
-			{output.map((item, index) => (
-				<pre key={index}>{item}</pre>
-			))}
+			<pre
+				style={{
+					margin: 0,
+					height: '100%',
+					boxSizing: 'border-box',
+					padding: '10px',
+					whiteSpace: 'pre-wrap',
+					width: '100%',
+				}}
+			>
+			{output.map((item) => item)}
+			</pre>
 		</div>
 	);
 }
