1bbae1a6d47e980c7b92200fdc91657e6ea5ba0e
Joseph Mearman <joseph@mearman.co.uk>
2023-11-21 16:48:05 +0000

    Update vite.config.ts
    - Added wasm plugin for Vite
    - Added topLevelAwait plugin for Vite
    - Updated build target to "esnext"
    - Updated preview and server configurations

--- samples/DevPulse/DevPulse React/vite.config.ts
+++ samples/DevPulse/DevPulse React/vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// import react from '@vitejs/plugin-react';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import open, { openApp, apps } from "open";
+
+
+const SERVER_PORT = 5173;
+async function openChrome() {
+	const args = [
+		// `http://localhost:${SERVER_PORT}`,
+		"--args",
+		"--disable-fre",
+		"--no-default-browser-check",
+		"--no-first-run",
+		`--user-data-dir=/tmp/${new Date().getDate()}`,
+		"--disable-web-security",
+		"--disable-gpu",
+		"--no-startup-window",
+		"--disable-site-isolation-trials",
+	];
+
+	return await openApp(apps.chrome, {
+		arguments: args,
+		background: true,
+		newInstance: false,
+		wait: false,
+	}).then(() => {
+		return "";
+	});
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [wasm(), react(), topLevelAwait()],
+	build: {
+		target: "esnext",
+	},
+	worker: {
+	},
+	preview: {
+		cors: false,
+		port: SERVER_PORT,
+		open: await openChrome(),
+	},
+
+	server: {
+		cors: false,
+		port: SERVER_PORT,
+		// cors: { origin: "*" },
+		open: await openChrome(),
+		strictPort: true,
+	},
 });
