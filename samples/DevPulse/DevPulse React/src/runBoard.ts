27d85b828d8f2fc713b8f929fb41754c55fceab7
Joseph Mearman <joseph@mearman.co.uk>
2023-11-22 06:24:12 +0000

    Add runBoard.ts file for Hacker News board
    This commit adds a new file, runBoard.ts, which contains the logic for running the Hacker News board. The file includes imports for necessary dependencies and sets up environment variables using dotenv. It also includes a function to generate a Markdown README file with specific configurations. Additionally, it includes a loop to handle input and output runs from the board, including special handling for the "claudeApiKey" input node and ignoring certain output nodes.

---
+++ samples/DevPulse/DevPulse React/src/runBoard.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env npm -y tsx watch
+import { MarkdownContentType } from "@exadev/breadboard-kits/types/markdown.js";
+import makeMarkdown from "@exadev/breadboard-kits/util/files/makeMarkdown.js";
+
+import dotenv from "dotenv";
+import board from "./hackerNewsBoard.ts";
+import { cleanString, ignoredOutputs } from "./TerminalOutput.tsx";
+
+dotenv.config({
+	path: ".env"
+});
+
+
+const CLAUDE_API_KEY = process.env.VITE_CLAUDE_API_KEY;
+
+makeMarkdown({
+	board,
+	filename: "README",
+	title: "Hacker News",
+	dir: ".",
+	markdownConfig: [
+		MarkdownContentType.mermaid,
+		MarkdownContentType.json
+	]
+});
+
+
+for await (const run of board.run()) {
+	if (run.type === "input") {
+		if (run.node.id === "claudeApiKey") {
+			run.inputs = {CLAUDE_API_KEY};
+		}
+	} else if (run.type === "output") {
+		if (ignoredOutputs.includes(run.node.id)) {
+			continue;
+		}
+		const outputMessage = cleanString(run);
+		console.log(outputMessage);
+	}
+}
+
