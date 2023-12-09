491757f907c2f1a5f575c41e9e200633fd6e804d
Joseph Mearman <joseph@mearman.co.uk>
2023-12-09 11:59:40 +0000

    Update store types with generic parameter
    - The `InputState` and `OutputState` types in the `store-types.ts` file have been updated to include a generic parameter.
    - The generic parameter is now set to `string`.
    - This change ensures that the state entities are associated with specific keys.

--- samples/DevPulse/src/hnStory/store-types.ts
+++ samples/DevPulse/src/hnStory/store-types.ts
@@ -2,5 +2,5 @@ import { EntityState } from "@reduxjs/toolkit";
 import { StoryOutput } from "~/hnStory/domain";
 import { WorkerData } from "~/sw/types";
 
-export type InputState = EntityState<WorkerData>;
-export type OutputState = EntityState<StoryOutput>;
+export type InputState = EntityState<WorkerData, string>;
+export type OutputState = EntityState<StoryOutput, string>;
