61e739b0651802c1a624f5a5b8deeb060260da8b
Joseph Mearman <joseph@mearman.co.uk>
2023-12-09 14:15:54 +0000

    Add service worker registration with different modes
    This commit adds the necessary code to register a service worker in the Vite Service Worker sample. The registration is now conditional based on the availability of the `serviceWorker` API. Additionally, different service worker files are used depending on the environment mode (`production` or `development`).

--- samples/vite-service-worker/src/main.tsx
+++ samples/vite-service-worker/src/main.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+
+// https://vite-pwa-org.netlify.app/guide/service-worker-without-pwa-capabilities.html#registering-of-the-service-worker-in-your-app
+
+// if ('serviceWorker' in navigator) {
+// 	navigator.serviceWorker.register(
+// 		import.meta.env.MODE === 'production' ? '/service-worker.js' : '/dev-sw.js?dev-sw'
+// 	);
+// }
+
+// If you're using import statements inside your service worker (will work only on chromium based browsers) check injectManifest section for more info
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register(
+		import.meta.env.MODE === 'production' ? '/service-worker.js' : '/dev-sw.js?dev-sw',
+		{ type: import.meta.env.MODE === 'production' ? 'classic' : 'module' }
+	);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
