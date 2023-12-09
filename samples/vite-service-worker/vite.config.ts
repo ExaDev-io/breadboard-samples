52220a41b0e2a302e7be5f366f31451b630836f3
Joseph Mearman <joseph@mearman.co.uk>
2023-12-09 14:15:18 +0000

    Add VitePWA plugin to vite.config.ts

--- samples/vite-service-worker/vite.config.ts
+++ samples/vite-service-worker/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		// vite.config.js or vite.config.ts
+		VitePWA({
+			srcDir: "src",
+			filename: "service-worker.ts",
+			strategies: "injectManifest",
+			injectRegister: false,
+			manifest: false,
+			devOptions: {
+				enabled: true,
+			},
+			injectManifest: {
+				injectionPoint: null,
+			},
+		}),
+	],
 });
