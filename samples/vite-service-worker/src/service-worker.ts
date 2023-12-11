7de909816a5b53376860b2d37d85d143b87c5495
Joseph Mearman <joseph@mearman.co.uk>
2023-12-11 09:25:44 +0000

    Add Board functionality to service worker

--- samples/vite-service-worker/src/service-worker.ts
+++ samples/vite-service-worker/src/service-worker.ts
@@ -1,4 +1,7 @@
 /// <reference lib="webworker" />
+
+import { Board } from "@google-labs/breadboard";
+
 declare let self: ServiceWorkerGlobalScope;
 
 self.addEventListener("fetch", (event: FetchEvent) => {
@@ -15,5 +18,34 @@ let counter = 0;
 setInterval(() => {
 	console.log(new Date().toISOString(), counter++);
 }, 1000);
+
+(async () => {
+	const board = new Board();
+	const output = board.output();
+	board.input({ $id: "one" }).wire("*", output);
+	board.input({ $id: "two" }).wire("*", output);
+	board.input({ $id: "three" }).wire("*", output);
+
+	let boardCounter = 0;
+
+	const LIMIT = 100;
+
+	while (boardCounter++ < LIMIT) {
+		for await (const runResult of board.run()) {
+			console.log("=".repeat(80));
+			console.log(runResult.node.id);
+
+			if (runResult.type == "input") {
+				runResult.inputs = {
+					time: new Date().toISOString(),
+				};
+			} else if (runResult.type == "output") {
+				console.log("output", runResult.outputs);
+			}
+		}
+	}
+})().catch((error) => {
+	console.error(error);
+});
 
 export {};
