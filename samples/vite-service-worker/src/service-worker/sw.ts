f19d96d6bcf474d03772c739b02be72af947b5bc
Joseph Mearman <joseph@mearman.co.uk>
2023-12-11 15:22:50 +0000

    Add service worker functionality and board control
    This commit adds functionality to the service worker by importing necessary modules and configuring precaching. It also initializes a board with input and output nodes, sets up a broadcast channel for communication, and handles commands to start, pause, or stop the board. Additionally, it implements an asynchronous run loop for the board with a handler function to process inputs and outputs.

--- samples/vite-service-worker/src/service-worker/sw.ts
+++ samples/vite-service-worker/src/service-worker/sw.ts
@@ -1,12 +1,14 @@
 /// <reference lib="webworker" />
 declare const self: ServiceWorkerGlobalScope;
 
-import { Board } from "@google-labs/breadboard";
+import { Board, RunResult } from "@google-labs/breadboard";
 import { precacheAndRoute } from "workbox-precaching";
 // import { registerRoute } from "workbox-routing";
+import { clientsClaim } from "workbox-core";
 
 precacheAndRoute(self.__WB_MANIFEST || []);
-
+self.skipWaiting();
+clientsClaim();
 // registerRoute(
 // 	({ request }) => request.mode === "navigate",
 // 	createHandlerBoundToURL("/index.html")
@@ -21,29 +23,89 @@ self.addEventListener("activate", () => {
 	return self.clients.claim();
 });
 
-let counter = 0;
-setInterval(() => {
-	console.log(new Date().toISOString(), counter++);
-}, 1000);
+const board = new Board();
+for (let i = 0; i < 10; i++) {
+	board
+		.input({ $id: `input_${i}` })
+		.wire("*", board.output({ $id: `output_${i}` }));
+}
 
-(async () => {
-	const board = new Board();
-	for (let i = 0; i < 100; i++) {
-		board.input({ $id: `input_${i}` }).wire("*", board.output());
+const channel = new BroadcastChannel("gen-control");
+channel.onmessage = (event) => handleCommand(event.data);
+
+let active = false;
+let paused = false;
+
+function handleCommand(data: { command: string }) {
+	console.log("ServiceWorker", "message", data);
+	switch (data.command) {
+		case "start":
+			if (!active) {
+				console.debug("ServiceWorker", "starting");
+				active = true;
+				paused = false;
+				runAsync(board.run(), handler).then(() => {
+					channel.postMessage({ command: "done" });
+				});
+			} else if (paused) {
+				console.debug("ServiceWorker", "resuming");
+				paused = false;
+				pausePromiseResolve();
+			} else {
+				console.warn("ServiceWorker", "already running");
+			}
+			break;
+		case "pause":
+			if (active) {
+				console.debug("ServiceWorker", "pausing");
+				paused = true;
+			} else {
+				console.warn("ServiceWorker", "not running");
+			}
+			break;
+		case "stop":
+			if (active) {
+				console.debug("ServiceWorker", "stopping");
+				active = false;
+				paused = false;
+				pausePromiseResolve();
+			} else {
+				console.warn("ServiceWorker", "not running");
+			}
+			break;
 	}
+}
 
-	for await (const runResult of board.run()) {
-		console.log("=".repeat(80));
-		console.log(runResult.node.id);
+let pausePromiseResolve: (value?: unknown) => void;
 
-		if (runResult.type == "input") {
-			runResult.inputs = {
-				message: new Date().toISOString(),
-			};
-		} else if (runResult.type == "output") {
-			console.log("output", runResult.outputs);
+async function runAsync(
+	run: AsyncGenerator<RunResult, unknown, unknown>,
+	handler: (value: RunResult) => unknown
+) {
+	for await (const runResult of run) {
+		if (!active) {
+			break;
 		}
+		if (paused) {
+			await new Promise((resolve) => {
+				pausePromiseResolve = resolve;
+			});
+		}
+		await handler(runResult);
 	}
-})().catch((error) => {
-	console.error(error);
-});
+	paused = false;
+	active = false;
+}
+
+async function handler(runResult: RunResult) {
+	if (runResult.type === "input") {
+		const input = {
+			time: new Date().toISOString(),
+		};
+		console.log(runResult.node.id, "input", input);
+		runResult.inputs = input;
+	} else if (runResult.type === "output") {
+		console.log(runResult.node.id, "output", runResult.outputs);
+	}
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+}
