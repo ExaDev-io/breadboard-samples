e010b84863de6c07eeb1bd37046df7d7bc4ae501
Joseph Mearman <joseph@mearman.co.uk>
2023-12-11 18:18:24 +0000

    Add ControllableAsyncGeneratorRunner import to sw.ts

--- samples/vite-service-worker/src/service-worker/sw.ts
+++ samples/vite-service-worker/src/service-worker/sw.ts
@@ -3,6 +3,7 @@ declare const self: ServiceWorkerGlobalScope;
 
 import { Board, RunResult } from "@google-labs/breadboard";
 import { precacheAndRoute } from "workbox-precaching";
+import { ControllableAsyncGeneratorRunner } from "../ControllableAsyncGeneratorRunner";
 
 precacheAndRoute(self.__WB_MANIFEST || []);
 
@@ -68,72 +69,4 @@ async function handler(runResult: RunResult): Promise<void> {
 		console.log(runResult.node.id, "output", runResult.outputs);
 	}
 	await new Promise((resolve): NodeJS.Timeout => setTimeout(resolve, 1000));
-}
-
-class ControllableAsyncGeneratorRunner<
-	TReturn,
-	TNext,
-	TNextReturn,
-	TGenerateParams
-> {
-	private pausePromiseResolve: undefined | ((value?: unknown) => void);
-	private paused: boolean = false;
-	private active: boolean = false;
-
-	constructor(
-		private readonly generatorGenerator: (
-			params?: TGenerateParams
-		) => AsyncGenerator<TReturn, TNext, TNextReturn>,
-		private readonly handler: (value: TReturn) => unknown,
-		private readonly generatorParams?: TGenerateParams
-	) {}
-
-	run(): void {
-		const generator = this.generatorGenerator(this.generatorParams);
-		const handler = this.handler;
-		this.active = true;
-		this.paused = false;
-		(async (): Promise<void> => {
-			try {
-				let next = await generator.next();
-				while (this.active && !next.done) {
-					if (this.paused) {
-						await new Promise((resolve): void => {
-							this.pausePromiseResolve = resolve;
-						});
-					}
-					await handler(next.value);
-					next = await generator.next();
-				}
-				console.debug("ServiceWorker", "generator done");
-			} catch (error) {
-				console.error(error);
-			} finally {
-				this.stop();
-			}
-		})();
-	}
-
-	start(): void {
-		if (!this.active) {
-			this.active = true;
-			this.paused = false;
-			this.run();
-		} else if (this.paused) {
-			this.paused = false;
-			this.pausePromiseResolve?.();
-		}
-	}
-
-	pause(): void {
-		this.paused = true;
-	}
-
-	stop() {
-		if (this.active) {
-			this.active = false;
-			this.paused = false;
-			this.pausePromiseResolve?.();
-		}
-	}
 }
