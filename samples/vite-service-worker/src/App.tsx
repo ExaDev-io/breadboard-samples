f8f14cc50a9dc211ee3931e00997d007738e6312
Joseph Mearman <joseph@mearman.co.uk>
2023-12-11 15:13:09 +0000

    Add control buttons to send commands to service worker
    This commit adds three buttons (Start, Pause, Stop) to the App component. Clicking on these buttons will send corresponding commands ('start', 'pause', 'stop') to the service worker via a broadcast channel.

--- samples/vite-service-worker/src/App.tsx
+++ samples/vite-service-worker/src/App.tsx
@@ -3,9 +3,19 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+const controlChannel = new BroadcastChannel('gen-control');
+function sendCommandToSW(command: string) {
+	// if (navigator.serviceWorker.controller) {
+	// 	navigator.serviceWorker.controller.postMessage({ command });
+	// 	console.log(`"${command}" -> SW`);
+	// } else {
+	// 	console.error('ServiceWorker not registered');
+	// }
+	controlChannel.postMessage({ command });
+}
+
 function App() {
 	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<div>
@@ -28,6 +38,11 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
+			<div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+				<button onClick={() => sendCommandToSW('start')}>Start</button>
+				<button onClick={() => sendCommandToSW('pause')}>Pause</button>
+				<button onClick={() => sendCommandToSW('stop')}>Stop</button>
+			</div>
 		</>
 	);
 }
