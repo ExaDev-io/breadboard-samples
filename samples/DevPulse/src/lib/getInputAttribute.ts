d4aca3ec666b2d53df4454da483efd165250679e
Joseph Mearman <joseph@mearman.co.uk>
2023-12-12 13:07:31 +0000

    Add CliAsyncGeneratorRunner and related functions
    - Added a new file `cliAsyncGeneratorRunner.ts` which contains the implementation of the `CliAsyncGeneratorRunner` class.
    - Added a new file `cliRunResultHandler.ts` which contains the implementation of the `cliRunResultHandler` function.
    - Added a new file `getInputAttribute.ts` which contains the implementation of the `getInputAttribute` function.
    - Added a new file `getInputForSchema.ts` which contains the implementation of the `getInputForSchema` function.
    - Modified the existing file `runBoard.ts` to import and use the newly added classes and functions.

---
+++ samples/DevPulse/src/lib/getInputAttribute.ts
@@ -0,0 +1,34 @@
+import { RunResult, Schema } from "@google-labs/breadboard";
+
+export function getInputAttribute(runResult: RunResult): Schema {
+	let schema: Schema;
+	const inputAttribute: string = runResult.state.newOpportunities.find(
+		(op) => op.from == runResult.node.id
+	)!.out!;
+
+	const schemaFromOpportunity = {
+		type: "object",
+		properties: {
+			[inputAttribute]: {
+				title: inputAttribute,
+				type: "string",
+			},
+		},
+	};
+
+	if (runResult.inputArguments.schema) {
+		schema = runResult.inputArguments.schema as Schema;
+		if (!Object.keys(schema.properties!).includes(inputAttribute)) {
+			throw new Error(
+				`Input attribute "${inputAttribute}" not found in schema:\n${JSON.stringify(
+					schema,
+					null,
+					2
+				)}`
+			);
+		}
+	} else {
+		schema = schemaFromOpportunity;
+	}
+	return schema;
+}
--- samples/DevPulse/src/lib/runBoard.ts
+++ samples/DevPulse/src/lib/runBoard.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env tsx
-import { Board, RunResult, Schema } from "@google-labs/breadboard";
-import board from "../breadboard/index";
-import * as readline from "readline";
-import { basename } from "path/posix";
 import { generateAndWriteCombinedMarkdown } from "@exadev/breadboard-kits/util/files/generateAndWriteCombinedMarkdown";
+import { RunResult } from "@google-labs/breadboard";
+import { CliAsyncGeneratorRunner } from "~/lib/cliAsyncGeneratorRunner.ts";
+import { cliRunResultHandler } from "~/lib/cliRunResultHandler.ts";
+import board from "../breadboard/index";
 
 // for await (const runResult of board.run()) {}
 
@@ -13,124 +13,12 @@ import { generateAndWriteCombinedMarkdown } from "@exadev/breadboard-kits/util/f
 // 	runResult = await boardRun.next();
 // }
 
-export class CliAsyncGeneratorRunner<
-	TReturn,
-	TNext,
-	TNextReturn,
-	TGenerateParams
-> {
-	constructor(
-		private readonly generatorGenerator: (
-			params?: TGenerateParams
-		) => AsyncGenerator<TReturn, TNext, TNextReturn | undefined>,
-		private readonly handler: (value: TReturn) => unknown,
-		private readonly generatorParams?: TGenerateParams
-	) {}
-	run(): void {
-		const generator = this.generatorGenerator(this.generatorParams);
-		const handler = this.handler;
-		(async (): Promise<void> => {
-			try {
-				for await (const value of generator) {
-					await handler(value);
-				}
-			} catch (error) {
-				console.error(error);
-			}
-		})();
-	}
-}
-
-function getInputAttribute(runResult: RunResult): Schema {
-	let schema: Schema;
-	const inputAttribute: string = runResult.state.newOpportunities.find(
-		(op) => op.from == runResult.node.id
-	)!.out!;
-
-	const schemaFromOpportunity = {
-		type: "object",
-		properties: {
-			[inputAttribute]: {
-				title: inputAttribute,
-				type: "string",
-			},
-		},
-	};
-
-	if (runResult.inputArguments.schema) {
-		schema = runResult.inputArguments.schema as Schema;
-		if (!Object.keys(schema.properties!).includes(inputAttribute)) {
-			throw new Error(
-				`Input attribute "${inputAttribute}" not found in schema:\n${JSON.stringify(
-					schema,
-					null,
-					2
-				)}`
-			);
-		}
-	} else {
-		schema = schemaFromOpportunity;
-	}
-	return schema;
-}
-
-function getInputForSchema(schema: Schema): Promise<{ [key: string]: string }> {
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-
-	const askQuestion = (query: string): Promise<string> => {
-		return new Promise((resolve) => rl.question(query, resolve));
-	};
-
-	async function getInputFromSchema() {
-		const userInput: { [key: string]: string } = {};
-
-		for (const key in schema.properties) {
-			const property = schema.properties[key];
-			if (property.type === "string") {
-				const answer = await askQuestion(`${property.title}: `);
-				userInput[key] = answer;
-			}
-		}
-
-		rl.close();
-		return userInput;
-	}
-
-	return getInputFromSchema();
-}
-
-export async function cliRunResultHandler(runResult: RunResult): Promise<void> {
-	console.debug("=".repeat(80));
-	console.debug(runResult.node.id, runResult.type);
-	if (runResult.type === "input") {
-		const inputAttribute = getInputAttribute(runResult);
-		const input = await getInputForSchema(inputAttribute);
-		console.debug(runResult.node.id, "input", input);
-		runResult.inputs = input;
-		return;
-	} else if (runResult.type === "output") {
-		console.debug(runResult.node.id, "output", runResult.outputs);
-		return;
-	}
-}
-
 generateAndWriteCombinedMarkdown({
 	board,
 	filename: "README",
 });
 
-const boardRunner: CliAsyncGeneratorRunner<
-	RunResult,
-	unknown,
-	unknown,
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	any
-> = new CliAsyncGeneratorRunner(
-	(): AsyncGenerator<RunResult, unknown, unknown> => board.run(),
+new CliAsyncGeneratorRunner(
+	(): AsyncGenerator<RunResult, unknown> => board.run(),
 	cliRunResultHandler
-);
-
-boardRunner.run();
+).run();
