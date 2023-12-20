c9496b8547ed998473f691d1a5a6c26f616d1e4b
Joseph Mearman <joseph@mearman.co.uk>
2023-12-20 09:02:52 +0000

    Add BreadboardComponent
    - Added a new file `BreadboardComponent.tsx` to the `src/components` directory.
    - The `BreadboardComponent` component renders a breadboard with input requests, service worker controller, and broadcast message renderer components.
    - This component replaces the previous content of the `App` component in `App.tsx`.
    - The new component is imported and used in the updated `App` component.

--- samples/DevPulse V4/src/components/App.tsx
+++ samples/DevPulse V4/src/components/App.tsx
@@ -1,47 +1,9 @@
-import viteLogo from "/vite.svg";
-import { ReactNode, useState } from 'react';
-import reactLogo from "~/assets/react.svg";
+import { ReactNode } from 'react';
 import "~/components/App.css";
-import { BroadcastMessageRenderer } from "~/components/BroadcastMessageRenderer.tsx";
-import { InputRequestsRenderer } from "~/components/InputRequestsRenderer.tsx";
-import { ServiceWorkerControllerComponent } from "~/components/ServiceWorkerControllerComponent.tsx";
-import { SW_BROADCAST_CHANNEL } from "~/lib/constants/SW_BROADCAST_CHANNEL.ts";
-import { BroadcastMessageType } from "~/lib/types/BroadcastMessageType.ts";
+import { BreadboardComponent } from "~/components/BreadboardComponent.tsx";
 
 function App(): ReactNode {
-	const [count, setCount] = useState(0);
-	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<InputRequestsRenderer channelId={SW_BROADCAST_CHANNEL} />
-			<ServiceWorkerControllerComponent />
-			<BroadcastMessageRenderer
-				channelId={SW_BROADCAST_CHANNEL}
-				ignoreMatchers={[
-					(message) => message.messageType != BroadcastMessageType.OUTPUT,
-				]}
-			/>
-		</>
-	);
+	return <BreadboardComponent/>
 }
 
 export default App;
---
+++ samples/DevPulse V4/src/components/BreadboardComponent.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import BroadcastMessageRenderer from "~/components/BroadcastMessageRenderer.tsx";
+import { InputRequestsRenderer } from "~/components/InputRequestsRenderer.tsx";
+import { ServiceWorkerControllerComponent } from "~/components/ServiceWorkerControllerComponent.tsx";
+import { SW_BROADCAST_CHANNEL } from "~/lib/constants";
+import { BroadcastMessageType } from "~/lib/types/BroadcastMessageType.ts";
+
+export function BreadboardComponent(): ReactNode {
+	return <div className="Breadboard">
+		<InputRequestsRenderer channelId={SW_BROADCAST_CHANNEL}/>
+		<ServiceWorkerControllerComponent/>
+		<BroadcastMessageRenderer
+			channelId={SW_BROADCAST_CHANNEL}
+			ignoreMatchers={[
+				(message) => message.messageType != BroadcastMessageType.OUTPUT,
+			]}
+		/>
+	</div>
+}
