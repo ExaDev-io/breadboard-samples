2497708b394639fcac339dbe0dfba8d2bef2027e
Joseph Mearman <joseph@mearman.co.uk>
2024-01-11 16:46:37 +0000

    Add search in progress message to BreadboardComponent
    - Added a new function `matchSearchInProgress` to check if the broadcast message is about a search in progress.
    - Created an array of matchers and added the `matchSearchInProgress` matcher to display a "Search in progress..." message.
    - Updated the `<BroadcastMessageRenderer>` component with the new `matchers` prop.

--- samples/DevPulse/src/lib/components/BreadboardComponent.tsx
+++ samples/DevPulse/src/lib/components/BreadboardComponent.tsx
@@ -1,11 +1,22 @@
 import { ReactNode } from "react";
+import { BroadcastMessage } from "~/lib/types/BroadcastMessage.ts";
 import { BroadcastMessageType } from "~/lib/types/BroadcastMessageType.ts";
-import { ServiceWorkerControllerComponent } from "./ServiceWorkerControllerComponent";
+import { SW_BROADCAST_CHANNEL } from "../constants";
 import BroadcastMessageRenderer from "./BroadcastMessageRenderer";
 import { InputRequestsRenderer } from "./InputRequestsRenderer";
-import { SW_BROADCAST_CHANNEL } from "../constants";
+import { ServiceWorkerControllerComponent } from "./ServiceWorkerControllerComponent";
 
+const matchSearchInProgress = (message: BroadcastMessage): boolean => {
+	return message.content != null && ("node" in (message.content as any)) && (message.content as any).node == "searchInProgress"
+}
 export function BreadboardComponent(): ReactNode {
+	let matchers: [((message: BroadcastMessage) => boolean), (() => JSX.Element)][] = [
+		[
+			(message) => matchSearchInProgress(message),
+			() => <div>Search in progress...</div>,
+		]
+	];
+
 	return (
 		<>
 			<ServiceWorkerControllerComponent />
@@ -15,6 +26,7 @@ export function BreadboardComponent(): ReactNode {
 				ignoreMatchers={[
 					(message) => message.messageType != BroadcastMessageType.OUTPUT,
 				]}
+				matchers={matchers}
 			/>
 		</>
 	);
