0155b33e4b38b0f359c86cdf5eb2102c9fb4bb98
Joseph Mearman <joseph@mearman.co.uk>
2024-01-11 17:48:34 +0000

    delete old files

--- samples/DevPulse/src/old/worker/workerControllerContext.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import useWorkerSteps from "~/hnStory/components/useWorkerSteps";
-import { WorkerControllerHook } from "~/old/worker/useWorkerController";
-
-export const WorkerControllerContext = React.createContext<{
-	broadcastChannel: WorkerControllerHook;
-	workerSteps: ReturnType<typeof useWorkerSteps>;
-} | null>(null);
