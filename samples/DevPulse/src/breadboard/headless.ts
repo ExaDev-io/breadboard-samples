715c59c72c011f9b807283fd414497f0832ae954
Joseph Mearman <joseph@mearman.co.uk>
2024-01-12 09:21:59 +0000

    Add generateAndWriteCombinedMarkdown function
    This commit adds the `generateAndWriteCombinedMarkdown` function to the codebase. It is imported from `@exadev/breadboard-kits/util/files/generateAndWriteCombinedMarkdown` and used in `headless.ts` to generate and write combined markdown files.

--- samples/DevPulse/src/breadboard/headless.ts
+++ samples/DevPulse/src/breadboard/headless.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env npx -y tsx
-import { LogProbe } from "@google-labs/breadboard";
+import { generateAndWriteCombinedMarkdown } from "@exadev/breadboard-kits/util/files/generateAndWriteCombinedMarkdown";
 import board from "~/breadboard/index.ts";
+
+generateAndWriteCombinedMarkdown({
+	board,
+	filename: "README",
+});
 
 (async () => {
 	for await (const runResult of board.run({
